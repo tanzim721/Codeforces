64443446753f0b400ce582a6779a3596df0b04cf
Black_idea <53827851+tanzim721@users.noreply.github.com>
2020-07-29 11:50:12 +0600

    Add files via upload

---
+++ Codeforces-A. A+B (Trial Problem).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,a,b,ans;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        ans = a+b;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
